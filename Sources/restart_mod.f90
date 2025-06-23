7cb0fb42214b64c9d0efb15a53136506faa2a882
cianciosa <m4c@ornl.gov>
2025-06-23 12:03:01 -0400

    Fix doxygen formatting.

--- Sources/restart_mod.f90
+++ Sources/restart_mod.f90
@@ -50,7 +50,7 @@
 !>     @item{phif_r_,  Toroidal flux profile.,                  island_params::phif_i}
 !>  @end_table
 !>
-!>  @table_sections{siesta_restart_2D_arrays_sec, 3D arrays.}
+!>  @table_section{siesta_restart_2D_arrays_sec, 3D arrays.}
 !>     @table_subsection{siesta_restart_internal_arrays_sec, Internal arrays}
 !>        @item{JBsupssh_m_n_r_, Normalized JB^s component sine parity.,   quantities::jbsupsmnsh}
 !>        @item{JBsupuch_m_n_r_, Normalized JB^u component cosine parity., quantities::jbsupumnch}
